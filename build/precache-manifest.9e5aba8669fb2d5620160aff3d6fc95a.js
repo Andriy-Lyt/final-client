730e205a4f747a8cc6723060a1769db8b70fd7e2
Andriy-Lyt <howdy_eh@icloud.com>
2022-02-21 20:57:41 -0500

    rebuilt build

---
+++ build/precache-manifest.9e5aba8669fb2d5620160aff3d6fc95a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842a051e5268844a55059942edde39d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9b4982aab6cdd9058c3",
+    "url": "/static/css/2.c1a0ad9b.chunk.css"
+  },
+  {
+    "revision": "3fa08772489d8fa0b5cf",
+    "url": "/static/css/main.8c87b56c.chunk.css"
+  },
+  {
+    "revision": "b9b4982aab6cdd9058c3",
+    "url": "/static/js/2.ac14b923.chunk.js"
+  },
+  {
+    "revision": "3fa08772489d8fa0b5cf",
+    "url": "/static/js/main.790263e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a2b3809c43e3a9608c035d5df672d7e5",
+    "url": "/static/media/avaya-logo-footer.a2b3809c.svg"
+  },
+  {
+    "revision": "45e7ddb92abef9badec81148099d0ad4",
+    "url": "/static/media/brand-new.45e7ddb9.svg"
+  },
+  {
+    "revision": "14cd814f39b9beaab4e97a9cc282d44b",
+    "url": "/static/media/design-foundations.14cd814f.svg"
+  },
+  {
+    "revision": "302f31561ade4da868de46efb23c600e",
+    "url": "/static/media/header-logo-full.302f3156.svg"
+  },
+  {
+    "revision": "806cbebcf6f5a0e325bf0e15648b50cb",
+    "url": "/static/media/home-image_one.806cbebc.svg"
+  },
+  {
+    "revision": "83fae2ed0cb06cf1f182dc7048008a19",
+    "url": "/static/media/icon-library.83fae2ed.svg"
+  },
+  {
+    "revision": "1730e10c7e0e6913384b089961a214ed",
+    "url": "/static/media/inclusive.1730e10c.svg"
+  },
+  {
+    "revision": "24c909d40407621472a17dfc83d8e142",
+    "url": "/static/media/ui-components.24c909d4.svg"
+  }
+]);
